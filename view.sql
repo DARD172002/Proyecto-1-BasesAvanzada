95a339cc05f7fdb3b57defaa6bd544e56b25abdc
DARD172002 <rayodev16@gmail.comn>
2025-01-02 11:50:41 -0600

    se agrega una vista para unificar la tabla usuario

---
+++ view.sql
@@ -0,0 +1,6 @@
+
+
+CREATE VIEW Unified_User AS SELECT
+u.ID_user, u.Name_User, U.Email, p.Password_User
+from Users u LEFT JOIN SQLServer_Node2.GlobalInventoryDBExample.dbo.Users p ON u.ID_user= p.ID_user
+Numero mam
